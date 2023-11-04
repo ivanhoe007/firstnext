d8b75a62f73a7e19281e3b9133ccba21ce1f9992
ivanhoe007 <sg018503@gmail.com>
2023-11-04 17:20:00 +0900

    fifth commit

--- app/edit/[id]/page.js
+++ app/edit/[id]/page.js
@@ -12,65 +12,137 @@ export default async function Edit(props) {
     const db = client.db('forum');
     let result = await db.collection('post').findOne({ _id: new ObjectId(props.params.id) });
     let session = await getServerSession(authOptions);
-    return (
-        <div className="wrap">
-            <div className="header">
-                <div className="header_center">
-                    <Link href={`/`}>
-                        <h1>logo</h1>
-                    </Link>
-                </div>
-            </div>
-            <div id="gray_line"></div>
-            <div className="main">
-                <div className="sidebar">
-                    <div className="sidebar_categories">
-                        <div className="sidebar_category LoginBtn hide">
-                            <Link href={`/login`}>
-                                <Button variant="primary">
-                                    <span>로그인</span>
-                                </Button>
-                            </Link>
-                        </div>
-                        <div className="sidebar_category SNSLoginBtn">
-                            <LoginBtn />
-                        </div>
-                        {session ? (
-                            <div className="sidebar_category logoutBtn">
-                                <LogoutBtn />
-                            </div>
-                        ) : (
-                            <div className="sidebar_category logoutBtn hide">
-                                <Button variant="primary">
-                                    <span>로그아웃</span>
-                                </Button>
-                            </div>
-                        )}
-                        <div className="sidebar_category">
-                            <Link href={`/detail`}>
-                                <Button variant="primary">
-                                    <span>게시판</span>
-                                </Button>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="p-20 page_into">
-                    <h4>수정페이지</h4>
-                    <form action="/api/post/edit" method="POST">
-                        제목
-                        <input name="title" defaultValue={result.title} style={{ width: 800 }} />
-                        <br />
-                        내용
-                        <input name="content" defaultValue={result.content} style={{ width: 800, height: 500 }} />
-                        <br />
-                        <input name="_id" defaultValue={result._id.toString()} className="hide" />
-                        <Button type="submit" variant="success">
-                            작성
-                        </Button>
-                    </form>
-                </div>
-            </div>
-        </div>
+    return ( <
+        div className = "wrap" >
+        <
+        div className = "header" >
+        <
+        div className = "header_center" >
+        <
+        Link href = { `/` } >
+        <
+        h1 > logo < /h1> <
+        /Link> <
+        /div> <
+        /div> <
+        div id = "gray_line" > < /div> <
+        div className = "main" >
+        <
+        div className = "sidebar" >
+        <
+        div className = "sidebar_categories" >
+        <
+        div className = "sidebar_category LoginBtn hide" >
+        <
+        Link href = { `/login` } >
+        <
+        Button variant = "primary" >
+        <
+        span > 로그인 < /span> <
+        /Button> <
+        /Link> <
+        /div>
+
+        {
+            session ? ( <
+                div className = "sidebar_category logoutBtn" >
+                <
+                LogoutBtn / >
+                <
+                /div>
+            ) : ( <
+                div className = "sidebar_category SNSLoginBtn" >
+                <
+                LoginBtn / >
+                <
+                /div>
+            )
+        } <
+        div className = "sidebar_category" >
+        <
+        Link href = { `/detail` } >
+        <
+        Button variant = "primary" >
+        <
+        span > 게시판 < /span> <
+        /Button> <
+        /Link> <
+        /div> {
+            session ? ( <
+                div className = "sidebar_category" >
+                <
+                Link href = { `/detail` } >
+                <
+                Button variant = "primary" >
+                <
+                span > 공지사항 < /span> <
+                /Button> <
+                /Link> <
+                /div>
+            ) : ( <
+                div className = "sidebar_category" >
+                <
+                AlertText text = "공지사항" / >
+                <
+                /div>
+            )
+        }
+
+        {
+            session ? ( <
+                div className = "sidebar_category" >
+                <
+                Link href = { `/detail` } >
+                <
+                Button variant = "primary" >
+                <
+                span > 청소구역 < /span> <
+                /Button> <
+                /Link> <
+                /div>
+            ) : ( <
+                div className = "sidebar_category" >
+                <
+                AlertText text = "청소구역" / >
+                <
+                /div>
+            )
+        }
+
+        <
+        /div> <
+        /div> <
+        div className = "p-20 page_into" >
+        <
+        h4 > 수정페이지 < /h4> <
+        form action = "/api/post/edit"
+        method = "POST" >
+        제목 <
+        input name = "title"
+        defaultValue = { result.title }
+        style = {
+            { width: 800 } }
+        /> <
+        br / >
+        내용 <
+        input name = "content"
+        defaultValue = { result.content }
+        style = {
+            { width: 800, height: 500 } }
+        /> <
+        br / >
+        <
+        input name = "_id"
+        defaultValue = { result._id.toString() }
+        className = "hide" / >
+        <
+        Button type = "submit"
+        variant = "success" >
+        작성 <
+        /Button> <
+        /form> <
+        /div> <
+        /div> <
+        /div>
     );
 }
--- app/write/page.js
+++ app/write/page.js
@@ -7,61 +7,130 @@ import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
 export default async function Write() {
     let session = await getServerSession(authOptions);
-    return (
-        <div className="wrap">
-            <div className="header">
-                <div className="header_center">
-                    <Link href={`/`}>
-                       <h1>logo</h1>
-                    </Link>
-                </div>
-            </div>
-            <div id="gray_line"></div>
-            <div className="main">
-                <div className="sidebar">
-                    <div className="sidebar_categories">
-                        <div className="sidebar_category LoginBtn hide">
-                            <Link href={`/login`}>
-                                <Button variant="primary">
-                                    <span>로그인</span>
-                                </Button>
-                            </Link>
-                        </div>
-                        <div className="sidebar_category SNSLoginBtn">
-                            <LoginBtn />
-                        </div>
-                        {session ? (
-                            <div className="sidebar_category logoutBtn">
-                                <LogoutBtn />
-                            </div>
-                        ) : (
-                            <div className="sidebar_category logoutBtn hide">
-                                <Button variant="primary">
-                                    <span>로그아웃</span>
-                                </Button>
-                            </div>
-                        )}
-                        <div className="sidebar_category">
-                            <Link href={`/detail`}>
-                                <Button variant="primary">
-                                    <span>게시판</span>
-                                </Button>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="p-20 page_into">
-                    <h4>글 작성</h4>
-                    <form action="/api/post/new" method="POST">
-                        <input className="article" name="title" placeholder="글제목" />
-                        <input className="article_content" name="content" placeholder="글내용" typeof='text' />
-                        <input type="file" name="" id=""/>
-                        <Button variant="success" type="submit">
-                            작성
-                        </Button>
-                    </form>
-                </div>
-            </div>
-        </div>
+    return ( <
+        div className = "wrap" >
+        <
+        div className = "header" >
+        <
+        div className = "header_center" >
+        <
+        Link href = { `/` } >
+        <
+        h1 > logo < /h1> <
+        /Link> <
+        /div> <
+        /div> <
+        div id = "gray_line" > < /div> <
+        div className = "main" >
+        <
+        div className = "sidebar" >
+        <
+        div className = "sidebar_categories" >
+        <
+        div className = "sidebar_category LoginBtn hide" >
+        <
+        Link href = { `/login` } >
+        <
+        Button variant = "primary" >
+        <
+        span > 로그인 < /span> <
+        /Button> <
+        /Link> <
+        /div>
+
+        {
+            session ? ( <
+                div className = "sidebar_category logoutBtn" >
+                <
+                LogoutBtn / >
+                <
+                /div>
+            ) : ( <
+                div className = "sidebar_category SNSLoginBtn" >
+                <
+                LoginBtn / >
+                <
+                /div>
+            )
+        } <
+        div className = "sidebar_category" >
+        <
+        Link href = { `/detail` } >
+        <
+        Button variant = "primary" >
+        <
+        span > 게시판 < /span> <
+        /Button> <
+        /Link> <
+        /div> {
+            session ? ( <
+                div className = "sidebar_category" >
+                <
+                Link href = { `/detail` } >
+                <
+                Button variant = "primary" >
+                <
+                span > 공지사항 < /span> <
+                /Button> <
+                /Link> <
+                /div>
+            ) : ( <
+                div className = "sidebar_category" >
+                <
+                AlertText text = "공지사항" / >
+                <
+                /div>
+            )
+        }
+
+        {
+            session ? ( <
+                div className = "sidebar_category" >
+                <
+                Link href = { `/detail` } >
+                <
+                Button variant = "primary" >
+                <
+                span > 청소구역 < /span> <
+                /Button> <
+                /Link> <
+                /div>
+            ) : ( <
+                div className = "sidebar_category" >
+                <
+                AlertText text = "청소구역" / >
+                <
+                /div>
+            )
+        } <
+        /div> <
+        /div> <
+        div className = "p-20 page_into" >
+        <
+        h4 > 글 작성 < /h4> <
+        form action = "/api/post/new"
+        method = "POST" >
+        <
+        input className = "article"
+        name = "title"
+        placeholder = "글제목" / >
+        <
+        input className = "article_content"
+        name = "content"
+        placeholder = "글내용"
+        typeof = 'text' / >
+        <
+        input type = "file"
+        name = ""
+        id = "" / >
+        <
+        Button variant = "success"
+        type = "submit" >
+        작성 <
+        /Button> <
+        /form> <
+        /div> <
+        /div> <
+        /div>
     );
 }
